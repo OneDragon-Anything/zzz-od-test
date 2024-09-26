e2e6ba198fafba700b6eb04cf23ef26c7ead022c
DoctorReid <doctorreid2024@outlook.com>
2024-09-26 23:14:42 +0800

    #246 修复血量扣减的识别

--- test/auto_battle/agent_state_checker/life_deduction/test_life_deduction.py
+++ test/auto_battle/agent_state_checker/life_deduction/test_life_deduction.py
@@ -1,6 +1,6 @@
 import test
 from zzz_od.auto_battle.agent_state import agent_state_checker
-from zzz_od.game_data.agent import CommonAgentStateEnum
+from zzz_od.game_data.agent import CommonAgentStateEnum, AgentStateDef
 
 
 class TestLifeDeduction(test.ZzzTestBase):
@@ -9,12 +9,23 @@ class TestLifeDeduction(test.ZzzTestBase):
         test.ZzzTestBase.__init__(self, *args, **kwargs)
 
     def test_life_deduction(self):
-        screen = self.get_test_image('life_0.png')
-        ans = agent_state_checker.check_length_by_foreground_color(self.ctx, screen, CommonAgentStateEnum.LIFE_DEDUCTION.value)
-        print(ans)
-        self.assertTrue(ans == 0)
+        for total in [1, 2, 3]:
+            for pos in [1]:
+                for cnt in [0, 5]:
+                    screen = self.get_test_image(f'{total}_{pos}_{cnt}')
+                    if screen is None:
+                        continue
+                    state = self._get_state(total, pos)
+                    ans = agent_state_checker.check_length_by_foreground_color(self.ctx, screen, state)
 
-        screen = self.get_test_image('life_gt_0.png')
-        ans = agent_state_checker.check_length_by_foreground_color(self.ctx, screen, CommonAgentStateEnum.LIFE_DEDUCTION.value)
-        print(ans)
-        self.assertTrue(ans > 0)
+                    print(total, pos, cnt, ans)
+                    if cnt > 0:
+                        self.assertTrue(ans >= cnt)
+                    else:
+                        self.assertEqual(cnt, ans)
+
+    def _get_state(self, total: int, pos: int) -> AgentStateDef:
+        if total == 3:
+            return CommonAgentStateEnum.LIFE_DEDUCTION_31.value
+        elif total in [1, 2]:
+            return CommonAgentStateEnum.LIFE_DEDUCTION_21.value
