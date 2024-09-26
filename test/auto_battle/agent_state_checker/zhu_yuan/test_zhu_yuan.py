41ce7a987c71ab5728d5fb825626419d3918c6fe
DoctorReid <doctorreid2024@outlook.com>
2024-09-26 22:20:34 +0800

    #246 修复朱鸢-子弹数的识别

--- test/auto_battle/agent_state_checker/zhu_yuan/test_zhu_yuan.py
+++ test/auto_battle/agent_state_checker/zhu_yuan/test_zhu_yuan.py
@@ -10,9 +10,17 @@ class TestZhuYuan(test.ZzzTestBase):
 
     def test_zhu_yuan(self):
         agent = AgentEnum.ZHU_YUAN.value
-        for i in [0, 1, 9]:
-            screen = self.get_test_image(f'zhu_yuan_{i}')
-            for state in agent.state_list:
-                ans = agent_state_checker.check_cnt_by_color_range(self.ctx, screen, state)
-                print(ans)
-                self.assertTrue(ans == i)
+        state = agent.state_list[0]
+
+        for total in [2, 3]:
+            for pos in [1, 2, 3]:
+                for cnt in [6, 9]:
+                    screen = self.get_test_image(f'{total}_{pos}_{cnt}')
+                    if screen is None:
+                        continue
+                    # if total != 3 or pos != 2 or cnt != 6:
+                    #     continue
+
+                    ans = agent_state_checker.check_cnt_by_color_range(self.ctx, screen, state, total, pos)
+                    print(total, pos, cnt, ans)
+                    self.assertEqual(cnt, ans)
