5989c2a5bdee9ab1442ebfc47a6dfc8f892ee394
DoctorReid <doctorreid2024@outlook.com>
2024-09-26 22:58:17 +0800

    #246 修复能量的识别

--- test/auto_battle/agent_state_checker/energy/test_energy.py
+++ test/auto_battle/agent_state_checker/energy/test_energy.py
@@ -1,6 +1,6 @@
 import test
 from zzz_od.auto_battle.agent_state import agent_state_checker
-from zzz_od.game_data.agent import CommonAgentStateEnum
+from zzz_od.game_data.agent import CommonAgentStateEnum, AgentStateDef
 
 
 class TestEnergy(test.ZzzTestBase):
@@ -8,113 +8,32 @@ class TestEnergy(test.ZzzTestBase):
     def __init__(self, *args, **kwargs):
         test.ZzzTestBase.__init__(self, *args, **kwargs)
 
-    def test_31(self):
-        screen = self.get_test_image('31_0.png')
-        ans = agent_state_checker.check_length_by_foreground_gray(self.ctx, screen, CommonAgentStateEnum.ENERGY_31.value)
-        print(ans)
-        self.assertTrue(ans <= 5)
+    def test_energy(self):
+        for total in [2, 3]:
+            for pos in [1, 2, 3]:
+                for cnt in [0, 60, 120]:
+                    screen = self.get_test_image(f'{total}_{pos}_{cnt}')
+                    if screen is None:
+                        continue
+                    state = self._get_state(total, pos)
+                    ans = agent_state_checker.check_length_by_foreground_gray(self.ctx, screen, state)
 
-        screen = self.get_test_image('31_lt_60.png')
-        ans = agent_state_checker.check_length_by_foreground_gray(self.ctx, screen, CommonAgentStateEnum.ENERGY_31.value)
-        print(ans)
-        self.assertTrue(ans < 60)  # 33
+                    print(total, pos, cnt, ans)
+                    if cnt > 0:
+                        self.assertTrue(ans >= cnt)
+                    else:
+                        self.assertEqual(cnt, ans)
 
-        screen = self.get_test_image('31_gt_60.png')
-        ans = agent_state_checker.check_length_by_foreground_gray(self.ctx, screen, CommonAgentStateEnum.ENERGY_31.value)
-        print(ans)
-        self.assertTrue(ans > 60)  # 62
-
-        screen = self.get_test_image('31_120.png')
-        ans = agent_state_checker.check_length_by_foreground_gray(self.ctx, screen, CommonAgentStateEnum.ENERGY_31.value)
-        print(ans)
-        self.assertTrue(ans == 120)
-
-    def test_32(self):
-        screen = self.get_test_image('32_0.png')
-        ans = agent_state_checker.check_length_by_foreground_gray(self.ctx, screen, CommonAgentStateEnum.ENERGY_32.value)
-        print(ans)
-        self.assertTrue(ans <= 5)
-
-        screen = self.get_test_image('32_lt_60.png')
-        ans = agent_state_checker.check_length_by_foreground_gray(self.ctx, screen, CommonAgentStateEnum.ENERGY_32.value)
-        print(ans)
-        self.assertTrue(ans < 60)  # 57
-
-        screen = self.get_test_image('32_gt_60.png')
-        ans = agent_state_checker.check_length_by_foreground_gray(self.ctx, screen, CommonAgentStateEnum.ENERGY_32.value)
-        print(ans)
-        self.assertTrue(ans > 60)  # 81
-
-        screen = self.get_test_image('32_120.png')
-        ans = agent_state_checker.check_length_by_foreground_gray(self.ctx, screen, CommonAgentStateEnum.ENERGY_32.value)
-        print(ans)
-        self.assertTrue(ans == 120)
-
-    def test_33(self):
-        screen = self.get_test_image('33_0.png')
-        ans = agent_state_checker.check_length_by_foreground_gray(self.ctx, screen, CommonAgentStateEnum.ENERGY_33.value)
-        print(ans)
-        self.assertTrue(ans <= 5)
-
-        screen = self.get_test_image('33_lt_60.png')
-        ans = agent_state_checker.check_length_by_foreground_gray(self.ctx, screen, CommonAgentStateEnum.ENERGY_33.value)
-        print(ans)
-        self.assertTrue(ans < 60)  # 58
-
-        screen = self.get_test_image('33_gt_60.png')
-        ans = agent_state_checker.check_length_by_foreground_gray(self.ctx, screen, CommonAgentStateEnum.ENERGY_33.value)
-        print(ans)
-        self.assertTrue(ans >= 60)  # 60
-
-        screen = self.get_test_image('33_120.png')
-        ans = agent_state_checker.check_length_by_foreground_gray(self.ctx, screen, CommonAgentStateEnum.ENERGY_33.value)
-        print(ans)
-        self.assertTrue(ans == 120)
-
-    def test_21(self):
-        screen = self.get_test_image('21_0.png')
-        ans = agent_state_checker.check_length_by_foreground_gray(self.ctx, screen, CommonAgentStateEnum.ENERGY_21.value)
-        print(ans)
-        self.assertTrue(ans <= 5)
-
-        screen = self.get_test_image('21_lt_60.png')
-        ans = agent_state_checker.check_length_by_foreground_gray(self.ctx, screen, CommonAgentStateEnum.ENERGY_21.value)
-        print(ans)
-        self.assertTrue(ans < 60)  # 32
-
-        screen = self.get_test_image('21_gt_60.png')
-        ans = agent_state_checker.check_length_by_foreground_gray(self.ctx, screen, CommonAgentStateEnum.ENERGY_21.value)
-        print(ans)
-        self.assertTrue(ans > 60)  # 75
-
-        screen = self.get_test_image('21_120.png')
-        ans = agent_state_checker.check_length_by_foreground_gray(self.ctx, screen, CommonAgentStateEnum.ENERGY_21.value)
-        print(ans)
-        self.assertTrue(ans == 120)
-
-    def test_22(self):
-        screen = self.get_test_image('22_0.png')
-        ans = agent_state_checker.check_length_by_foreground_gray(self.ctx, screen, CommonAgentStateEnum.ENERGY_22.value)
-        print(ans)
-        self.assertTrue(ans <= 5)
-
-        screen = self.get_test_image('22_lt_60.png')
-        ans = agent_state_checker.check_length_by_foreground_gray(self.ctx, screen, CommonAgentStateEnum.ENERGY_22.value)
-        print(ans)
-        self.assertTrue(ans < 60)  # 32
-
-        screen = self.get_test_image('22_gt_60.png')
-        ans = agent_state_checker.check_length_by_foreground_gray(self.ctx, screen, CommonAgentStateEnum.ENERGY_22.value)
-        print(ans)
-        self.assertTrue(ans > 60)  # 75
-
-        screen = self.get_test_image('22_120.png')
-        ans = agent_state_checker.check_length_by_foreground_gray(self.ctx, screen, CommonAgentStateEnum.ENERGY_22.value)
-        print(ans)
-        self.assertTrue(ans == 120)
-
-    def test_11(self):
-        screen = self.get_test_image('11_120.png')
-        ans = agent_state_checker.check_length_by_foreground_gray(self.ctx, screen, CommonAgentStateEnum.ENERGY_21.value)
-        print(ans)
-        self.assertTrue(ans == 120)
+    def _get_state(self, total: int, pos: int) -> AgentStateDef:
+        if total == 3:
+            if pos == 1:
+                return CommonAgentStateEnum.ENERGY_31.value
+            elif pos == 2:
+                return CommonAgentStateEnum.ENERGY_32.value
+            elif pos == 3:
+                return CommonAgentStateEnum.ENERGY_33.value
+        elif total == 2:
+            if pos == 1:
+                return CommonAgentStateEnum.ENERGY_21.value
+            elif pos == 2:
+                return CommonAgentStateEnum.ENERGY_22.value
