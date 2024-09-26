c0ca335269fe5fb9ad516346d1647ea2dc851524
DoctorReid <doctorreid2024@outlook.com>
2024-09-26 22:34:59 +0800

    #246 修复赛斯-意气的识别

--- test/auto_battle/agent_state_checker/seth_lowell/test_seth_lowell.py
+++ test/auto_battle/agent_state_checker/seth_lowell/test_seth_lowell.py
@@ -9,13 +9,22 @@ class TestSethLowell(test.ZzzTestBase):
         test.ZzzTestBase.__init__(self, *args, **kwargs)
 
     def test_seth_lowell(self):
-        """
-        赛斯
-        """
         agent = AgentEnum.SETH_LOWELL.value
-        for i in ['0', '75', '100']:
-            screen = self.get_test_image(f'seth_lowell_{i}')
-            for state in agent.state_list:
-                ans = agent_state_checker.check_length_by_background_gray(self.ctx, screen, state)
-                print(ans)
-                self.assertTrue(ans >= int(i))
+        state = agent.state_list[0]
+
+        for total in [2, 3]:
+            for pos in [1, 2, 3]:
+                for cnt in [0, 75, 100]:
+                    screen = self.get_test_image(f'{total}_{pos}_{cnt}')
+                    if screen is None:
+                        continue
+                    # if total != 3 or pos != 1 or cnt != 3:
+                    #     continue
+
+                    ans = agent_state_checker.check_length_by_background_gray(self.ctx, screen, state, total, pos)
+
+                    print(total, pos, cnt, ans)
+                    if cnt > 0:
+                        self.assertTrue(ans >= cnt)
+                    else:
+                        self.assertEqual(cnt, ans)
