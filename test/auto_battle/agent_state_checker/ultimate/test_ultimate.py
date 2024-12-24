536ef605ee3d3f29d228c23f420a9564105940ce
DoctorReid <doctorreid2024@outlook.com>
2024-12-24 23:35:56 +0800

    #507 增加状态 **-终结技可用

---
+++ test/auto_battle/agent_state_checker/ultimate/test_ultimate.py
@@ -0,0 +1,38 @@
+import test
+from zzz_od.auto_battle.agent_state import agent_state_checker
+from zzz_od.game_data.agent import AgentEnum, CommonAgentStateEnum, AgentStateCheckWay
+
+
+class TestUltimate(test.ZzzTestBase):
+
+    def __init__(self, *args, **kwargs):
+        test.ZzzTestBase.__init__(self, *args, **kwargs)
+
+    def test_zhu_yuan(self):
+        state_map = {
+            2: [CommonAgentStateEnum.ULTIMATE_21.value, CommonAgentStateEnum.ULTIMATE_22.value],
+            3: [CommonAgentStateEnum.ULTIMATE_31.value, CommonAgentStateEnum.ULTIMATE_32.value, CommonAgentStateEnum.ULTIMATE_33.value]
+        }
+
+        for agent_num in [2, 3]:
+            for ultimate_pos in [0, 1, 2, 3]:
+                screen = self.get_test_image(f'{agent_num}_{ultimate_pos}')
+                if screen is None:
+                    continue
+
+                if agent_num not in state_map:
+                    continue
+
+                state_list = state_map[agent_num]
+
+                for state_idx in range(len(state_list)):
+                    agent_pos = state_idx + 1
+                    state = state_list[state_idx]
+
+                    if state.check_way == AgentStateCheckWay.COLOR_RANGE_EXIST:
+                        ans = agent_state_checker.check_exist_by_color_range(self.ctx, screen, state)
+                    elif state.check_way == AgentStateCheckWay.TEMPLATE_NOT_FOUND:
+                        ans = agent_state_checker.check_template_not_found(self.ctx, screen, state)
+
+                    print(agent_num, ultimate_pos, agent_pos, ans)
+                    self.assertEqual(ultimate_pos == agent_pos, ans)
