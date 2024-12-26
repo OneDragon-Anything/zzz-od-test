cf26d2bd3e109f311f3bdab415700ccce53c9d72
DoctorReid <doctorreid2024@outlook.com>
2024-12-26 22:12:07 +0800

    #465 优化部分快捷手册的选tab

--- test/test_get_match_screen_name/test_get_match_screen_name.py
+++ test/test_get_match_screen_name/test_get_match_screen_name.py
@@ -10,10 +10,11 @@ class TestGetMatchScreenName(test.ZzzTestBase):
     def test(self):
         screen_map = {
             'normal_world_basic': '大世界-普通',
-            'menu_entry': '菜单-入口',
+            'menu': '菜单',
             'storage_wengine': '仓库-音擎仓库',
             'storage_drive_disc': '仓库-驱动仓库',
             'drive_disc_dismantle': '仓库-驱动仓库-驱动盘拆解',
+            'compendium_train': '快捷手册-训练'
         }
         for image_name, screen_name in screen_map.items():
             screen = self.get_test_image(image_name)
