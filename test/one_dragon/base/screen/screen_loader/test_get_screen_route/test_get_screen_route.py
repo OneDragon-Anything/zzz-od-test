f6809e4c1ffa6978c507d31de08bcf3bab03d36e
yijie.mo <yijie.mo@vipshop.com>
2024-12-27 14:29:13 +0800

    #547 修复防卫战和空洞的快捷手册入口

---
+++ test/one_dragon/base/screen/screen_loader/test_get_screen_route/test_get_screen_route.py
@@ -0,0 +1,13 @@
+import test
+from one_dragon.base.screen import screen_utils
+
+
+class TestGetScreenRote(test.ZzzTestBase):
+
+    def __init__(self, *args, **kwargs):
+        test.ZzzTestBase.__init__(self, *args, **kwargs)
+
+    def test_get_route(self):
+        route = self.ctx.screen_loader.get_screen_route('快捷手册-训练', '快捷手册-战术')
+        self.assertIsNotNone(route)
+        self.assertTrue(route.can_go)
