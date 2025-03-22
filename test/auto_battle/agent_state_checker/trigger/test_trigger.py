a584fbbe4734201418056d3a26a2eefa7bc73c24
DoctorReid <doctorreid2024@outlook.com>
2025-03-22 15:17:10 +0800

    #861 扳机识别

--- test/auto_battle/agent_state_checker/trigger/test_trigger.py
+++ test/auto_battle/agent_state_checker/trigger/test_trigger.py
@@ -3,22 +3,24 @@ from zzz_od.auto_battle.agent_state import agent_state_checker
 from zzz_od.game_data.agent import AgentEnum
 
 
-class TestBurniceWhite(test.ZzzTestBase):
+class TestTrigger(test.ZzzTestBase):
 
     def __init__(self, *args, **kwargs):
         test.ZzzTestBase.__init__(self, *args, **kwargs)
 
-    def test_burnice_white(self):
-        agent = AgentEnum.BURNICE_WHITE.value
+    def test_trigger(self):
+        agent = AgentEnum.TRIGGER.value
         state = agent.state_list[0]
 
         for total in [2, 3]:
             for pos in [1, 2, 3]:
-                for l in [0, 60, 100]:
+                for l in [0, 100]:
+                    # if not (total == 3 and pos == 3 and l == 100): # debug
+                    #     continue
                     screen = self.get_test_image(f'{total}_{pos}_{l}')
                     if screen is None:
                         continue
 
-                    ans = agent_state_checker.check_length_by_background_gray(self.ctx, screen, state, total, pos)
+                    ans = agent_state_checker.check_length_by_foreground_color(self.ctx, screen, state, total, pos)
                     print(total, pos, l, ans)
-                    self.assertTrue(abs(l - ans) <= 5)
+                    self.assertTrue(abs(l - ans) < 5)
