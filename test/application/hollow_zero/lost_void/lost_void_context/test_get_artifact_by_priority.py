a50f6f0caa746940ab7eca1b0ddafb3efb5b2ce0
DoctorReid <doctorreid2024@outlook.com>
2025-06-14 22:27:08 +0800

    feat: 迷失之地 邦布商店加入NEW!判断 加入右滑看跟多商品 #1095

--- test/application/hollow_zero/lost_void/lost_void_context/test_get_artifact_by_priority.py
+++ test/application/hollow_zero/lost_void/lost_void_context/test_get_artifact_by_priority.py
@@ -1,5 +1,7 @@
 import test
+from one_dragon.base.geometry.rectangle import Rect
 from one_dragon.base.matcher.match_result import MatchResult
+from zzz_od.application.hollow_zero.lost_void.operation.interact.lost_void_artifact_pos import LostVoidArtifactPos
 
 
 class TestLostVoidContext(test.ZzzTestBase):
@@ -24,34 +26,34 @@ class TestLostVoidContext(test.ZzzTestBase):
 
         self.ctx.lost_void.challenge_config.update(
             'artifact_priority_2',
-            ['异常·击破 混沌量杯', '通用三相战斗陀螺', '绝境'],
+            ['异常·击破 混沌量杯', '通用 三相战斗陀螺', '绝境'],
             save=False
         )
 
-        art_name_list = ['卡牌 超越绝境之志', '终结 蓄势架子鼓', '异常·击破 万能催化剂', '机敏 高效保险杠']
-        art_list = []
+        art_name_list = ['超越绝境之志', '[终结]蓄势架子鼓', '[异常·击破]万能催化剂', '[机敏]高效保险杠']
+        art_list: list[LostVoidArtifactPos] = []
         for art_name in art_name_list:
-            art = self.ctx.lost_void.match_artifact_by_ocr_full(art_name)
-            mr = MatchResult(1, 0, 0, 0, 0, data=art)
-            art_list.append(mr)
+            art = self.ctx.lost_void.get_artifact_by_full_name(art_name)
+            pos = LostVoidArtifactPos(art, Rect(0, 0, 0, 0))
+            art_list.append(pos)
 
-        priority_list = self.ctx.lost_void.get_artifact_by_priority(
+        priority_list: list[LostVoidArtifactPos] = self.ctx.lost_void.get_artifact_by_priority(
             art_list, 1,
             consider_priority_1=True, consider_priority_2=False,
             consider_not_in_priority=False)
         self.assertEqual(1, len(priority_list))
-        self.assertEqual('终结 蓄势架子鼓', priority_list[0].data.display_name)
+        self.assertEqual('[终结]蓄势架子鼓', priority_list[0].artifact.display_name)
 
         priority_list = self.ctx.lost_void.get_artifact_by_priority(
             art_list, 1,
             consider_priority_1=True, consider_priority_2=True,
             consider_not_in_priority=False)
         self.assertEqual(1, len(priority_list))
-        self.assertEqual('终结 蓄势架子鼓', priority_list[0].data.display_name)
+        self.assertEqual('[终结]蓄势架子鼓', priority_list[0].artifact.display_name)
 
         priority_list = self.ctx.lost_void.get_artifact_by_priority(
             art_list, 1,
             consider_priority_1=True, consider_priority_2=True,
             consider_not_in_priority=True)
         self.assertEqual(1, len(priority_list))
-        self.assertEqual('终结 蓄势架子鼓', priority_list[0].data.display_name)
+        self.assertEqual('[终结]蓄势架子鼓', priority_list[0].artifact.display_name)
